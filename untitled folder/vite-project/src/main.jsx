c1a1484d15445a0cecf7a3faa399f04aa3e6b162
shivamsiingh515 <singhajayshivam2006@gmail.com>
2026-04-01 22:13:45 +0530

    fetched api and displayed it on website

--- untitled folder/vite-project/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import {createRoot} from "react-dom/client"
-function app(){
-    4
-    return <img src="/assets/neumann.png" alt="neumann"/>
-
-}
-
-
-createRoot(document.getElementById("neumann")).render(c)
